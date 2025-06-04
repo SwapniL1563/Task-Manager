b3dedd15f4c2e4f1e5b7cfea19216d31c787a1ac
SwapniL1563 <choratswapnil15@gmail.com>
2025-06-04 17:31:27 +0530

    Deployment ready

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,19 @@ const PORT = 3000;
 const app = express();
 
 dotenv.config()
-app.use(express.json());
+
 app.use(cors({
     origin: 'https://task-manager-frontend-one-ebon.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-}));
+  }));
   
+  app.options('*', cors()); 
 
+  app.use(express.json());
+  
+// connect to db
 connectDb();
 
 // api endpoints
